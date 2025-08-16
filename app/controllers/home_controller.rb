b11426460ab49cd71a7a3387d993bbc621019932
Sean Dickinson <sean@bythepixel.com>
2025-08-16 15:08:06 -0400

    feat: home page test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,18 @@
 class HomeController < ApplicationController
   def index
-    render inertia: 'Home/Index'
+    render inertia: "Home/Index", props: {
+      current_puzzle:
+    }
+  end
+
+  private
+
+  def current_puzzle
+    puzzle = Puzzle.find_by!(date: Date.current.to_s)
+
+    {
+      card: puzzle.cards.first,
+      date: puzzle.date.to_s
+    }
   end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,15 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
-    assert_response :success
+    freeze_time do
+      cards = SetGame::DeckBuilder.new.build_deck.sample(12)
+      expected_card = cards.first.as_json
+      Puzzle.create!(cards:, date: Date.current.to_s)
+
+      get home_index_url
+
+      assert_response :success
+      assert_props current_puzzle: { card: expected_card, date: Date.current.to_s }
+    end
   end
 end
