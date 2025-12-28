ea164c5dcb818465e559ffe5a54500d5834d8602
Sean Dickinson <sean@bythepixel.com>
2025-12-28 16:09:17 -0500

    feat: add helper fns

--- lib/set_game/card.rb
+++ lib/set_game/card.rb
@@ -1,5 +1,39 @@
 module SetGame
   Card = Data.define(*ATTRIBUTES) do
+    class << self
+      def from(string_or_hash)
+        case string_or_hash
+        when String then from_id(string_or_hash)
+        when Hash then from_hash(string_or_hash)
+        else
+          raise ArgumentError, "Expected String or Hash, got #{string_or_hash.class}"
+        end
+      end
+
+      private
+
+      def from_id(id)
+        number, shade, color, shape = id.split("-")
+        new(
+          number: number.to_i,
+          shade: shade.to_sym,
+          color: color.to_sym,
+          shape: shape.to_sym
+        )
+      end
+
+      def from_hash(hash)
+        transformed = hash.transform_values do
+          if it.is_a? Integer
+            it
+          else
+            it.to_sym
+          end
+        end
+        new(**transformed)
+      end
+    end
+
     def to_s
       base = "#{number} #{shade} #{color} #{shape}"
       if number > 1
@@ -7,6 +41,14 @@ module SetGame
       else
         base
       end
+    end
+
+    def id
+      [ number, shade, color, shape ].join("-")
+    end
+
+    def ==(other)
+      ATTRIBUTES.all? { |attribute| send(attribute) == other.send(attribute) }
     end
   end
 end
