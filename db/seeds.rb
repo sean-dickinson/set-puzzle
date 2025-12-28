f18191d734910d6bef518b5feecfc9f36d579188
Sean Dickinson <sean@bythepixel.com>
2025-12-28 15:05:21 -0500

    feat: seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+current_environment = Rails.env.downcase
+puts "Current environment: #{current_environment}"
+puts "Running seeds!"
+start_time = Time.current
+load(Rails.root.join('db', 'seeds', "#{current_environment}.rb"))
+end_time = Time.current
+puts "Finished seeding in #{end_time - start_time} seconds"
